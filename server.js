3e444f988aa3cd7eed8da070223a2ef65b49c01d
Laurentino Dantas <laurentino.dantas@usp.br>
2022-03-29 23:45:26 -0300

    Versao com participante circulo e botoes de notificacao e alerta

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/espim-front-col'));
+app.use(express.static('./dist/espim_front_col'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/espim-front-col/'}),
+    res.sendFile('index.html', {root: 'dist/espim_front_col/'}),
 );
 
 // Start the app by listening on the default Heroku port
